e683716f8a57c62c0cf612315b013778bf2b56a5
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2013-09-23 00:02:53 -0300

    kick-off

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contabilidade::Application.config.secret_key_base = 'a6e447c060da4c19132e0fbec597dcbf9eb81df7e08273e8ab224306be9d5ca98402833b9e14fc36ebddc174a81c82db4c7b055ffd338e8d7e8adc1f769712da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Contabilidade::Application.config.session_store :cookie_store, key: '_contabilidade_session'
